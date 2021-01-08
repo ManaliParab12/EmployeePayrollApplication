d78f7b77755458fc45d02d251f0555c1b01dfd35
ManaliParab12 <manaliparab10@gmail.com>
2021-01-08 21:34:32 +0530

    made use of local storage false in site_properties.js

--- Js/site_properties.js
+++ Js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../Pages/home.html",
     add_emp_payroll_page: "../Pages/Payroll_form.html",
     server_url: "http://localhost:3000/EmployeePayrollDB/"
