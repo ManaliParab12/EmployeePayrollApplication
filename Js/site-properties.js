242db9f919063d74b589f4e484bba4dc9a3606be
ManaliParab12 <manaliparab10@gmail.com>
2021-01-08 17:15:57 +0530

    added server url and defined local storage variable to make status true or false in site-properties.js

--- Js/site-properties.js
+++ Js/site-properties.js
@@ -1,4 +1,7 @@
 let site_properties = {
+    use_local_storage: "true",
     home_page: "../Pages/home.html",
-    add_emp_payroll_page: "../Pages/Payroll_form.html"
+    add_emp_payroll_page: "../Pages/Payroll_form.html",
+    server_url: "http://localhost:3000/EmployeePayrollDB/"
+
 };
